34243771e33db2128bf36dcd1663d4665e41ff56
rhojn <rhojeno8@gmail.com>
2018-09-07 20:04:40 +0800

    coded update to page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './style.css';
 
+import githubMark from '../../assets/images/github-mark.png';
+
 import Container from '../Container';
 
 class Header extends Component {
@@ -10,6 +12,7 @@ class Header extends Component {
       <header className={ `header ${mode}` }>
         <Container>
           <div className="brand"><a href="/">Wiki</a></div>
+          <div className="social"><a href="https://github.com/rhojn/wiki-react" rel="noreferrer noopener" target="_blank"><img src={githubMark} alt="Github Mark" /></a></div>
         </Container>
       </header>
     );
--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -7,14 +7,14 @@ class Pagination extends Component {
     return (
       <div className="pagination">
         <ul className="pagination-wrapper">
-          { page > 1 ? (<li className="pagination-item"><span data-page={page - 1} onClick={handlePageChange}>PREV</span></li>) : null }
+          { page > 1 ? (<li className="pagination-item"><span data-page={page - 1} onClick={handlePageChange}>Previous</span></li>) : null }
           { Array.from(Array(Math.ceil(pages)), (e, i) => {
             const numPage = i+1;
             return (
               <li className={`pagination-item ${Number(page) === Number(numPage) ? "is-active" : ""}`} key={numPage}><span data-page={numPage} onClick={handlePageChange}>{numPage}</span></li>
             );
           })}
-          { page < pages ? (<li className="pagination-item"><span data-page={page + 1} onClick={handlePageChange}>NEXT</span></li>) : null }
+          { page < pages ? (<li className="pagination-item"><span data-page={page + 1} onClick={handlePageChange}>Next</span></li>) : null }
         </ul>
       </div>
     )
